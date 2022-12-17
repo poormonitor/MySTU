478eed6050d65df6ca1f132da8930abb9efd9277
poormonitor <poormonitor@outlook.com>
2022-12-17 19:20:26 +0800

    fix route match

--- view/src/views/Admin.vue
+++ view/src/views/Admin.vue
@@ -1,15 +1,19 @@
 <script setup>
+import { computed } from "vue"
+import { useRoute } from "vue-router"
 import { IconFont } from 'tdesign-icons-vue-next';
 const mngtPath = [
     { to: { name: "userAdmin" }, text: "用户管理", icon: "usergroup-add" },
     { to: { name: "uploadExcel" }, text: "上传数据", icon: "cloud-upload" }
 ]
+const route = useRoute()
+const currentRoute = computed(() => route.name)
 </script>
 
 <template>
     <div class="h-full flex flex-row divide-x">
         <div id="adminNavi" class="flex overflow-x-hidden basis-1/4 md:basis-1/6">
-            <t-menu theme="light" :value="this.$route.name">
+            <t-menu theme="light" :value="currentRoute">
                 <t-menu-item :value="path.to.name" :to="path.to" v-for="path in mngtPath">
                     {{ path.text }}
                     <template #icon>
